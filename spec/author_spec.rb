93a9757c03c0b0a298d591b16f2af6d0e8f2f4a1
Will Weston <Williamweston81@gmail.com>
2023-07-28 12:19:32 -0500

    Weston author attributes

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -6,3 +6,11 @@ describe Author do
   before :each do
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   end
+
+  it 'exists with attributes' do
+    expect(@charlotte_bronte).to be_a Author
+    expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
+    expect(@charlotte_bronte.books).to eq([])
+  end
+
+end
