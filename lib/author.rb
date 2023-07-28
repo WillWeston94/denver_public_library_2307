c4182c45956592bf2d92117a678e094b68d37cb4
Will Weston <Williamweston81@gmail.com>
2023-07-28 12:54:08 -0500

    Weston spelling mistake on villete instead of villette crushed my time

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,5 @@
+require './lib/book'
+
 class Author
     attr_reader :first_name,
                 :last_name,
@@ -13,16 +15,21 @@ class Author
       "#{@first_name} #{@last_name}"
     end
 
-    def books
-      @books
-    end
-
     def write(title, publication_date)
+      publication_year = extract_publication_year(publication_date)
+
       book = Book.new({
-      first_name: first_name,
-      last_name: last_name,
+      author_first_name: first_name,
+      author_last_name: last_name,
       title: title,
       publication_date: publication_date
       })
+
+      @books << book
+      book
+    end
+
+    def extract_publication_year(date)
+      date.split(',').last.strip[-4..-1]
     end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,7 +14,19 @@ describe Author do
   end
 
   it 'jane_eyre exists as book' do
-    require 'pry' ; binding.pry
-    expect(@jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")).to eq()
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # require 'pry' ; binding.pry
+    expect(@jane_eyre).to be_a(Book)
+    expect(@jane_eyre.title).to eq("Jane Eyre")
+    expect(@charlotte_bronte.books).to include(@jane_eyre)
+  end
+
+  it 'villete exists as book' do
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+    # require 'pry' ; binding.pry
+    expect(@villette).to be_a(Book)
+    expect(@villette.title).to eq("Villette")
+    expect(@charlotte_bronte.books).to include(@jane_eyre, @villette)
   end
 end
